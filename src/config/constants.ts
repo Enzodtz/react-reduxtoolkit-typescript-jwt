82e951dac11c1ac009228b751241e4adef440bd0
Enzodtz <enzodtz@gmail.com>
2022-07-20 04:47:48 -0300

    Fixing backend link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://bycavallinixx-backend.herokuapp.com/";
 const REFRESH_EXPIRY = 5000; // TODO: Fix refresh expiry, load from jwt
 
 export default { API_BASE_URL, REFRESH_EXPIRY };
